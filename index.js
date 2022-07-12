b8d7b65923f5726f94391c39ab2b15995f9c8bde
ovovsyankina <olgaovsyankina.ovsyankina@yandex.ru>
2022-07-12 18:32:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+let arr = [];
+let todoItems = document.querySelector(".list_items");
+let inputText = document.querySelector(".input_text");
+let todoCheck = document.querySelector(".todo_checkbox");
+let textTask = document.querySelector(".textTask");
+let filter = "all";
+let count = document.querySelector(".count");
+function createElement() {
+  todoElem = "";
+
+  arr.forEach((todo, i) => {
+    let id = i + new Date().getTime();
+    arr[i].id = id;
+    todoElem += `<li class="todo"  id="todo_${id}" ><input type="checkbox" class="todo_checkbox" id="todoCheck_${id}" onchange="checkElement(${id})" ${
+      todo.complete ? "checked" : ""
+    }><div class="textTask"  id="todoText_${id}">${todo.text}</div> </li>`;
+    todoItems.innerHTML = todoElem;
+  });
+  count.innerHTML = counts() + "  items left";
+  // console.log(arr);
+}
+
+document.querySelector("input").addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    newElement();
+    inputText.value = "";
+  }
+});
+function newElement() {
+  let infoTodo = {
+    text: inputText.value,
+    id: 1,
+    complete: false,
+  };
+  if (inputText.value !== "") {
+    arr.push(infoTodo);
+    createElement();
+  }
+}
+
+function checkElement(id) {
+  let todoCheck_elemId = document.querySelector(`#todoCheck_${id}`);
+  if (todoCheck_elemId.checked) {
+    arr.forEach((todo) => {
+      if (todo.id === id) {
+        todo.complete = true;
+      }
+    });
+  } else {
+    arr.forEach((todo) => {
+      if (todo.id === id) {
+        todo.complete = false;
+      }
+    });
+  }
+  console.log(arr);
+  createElement();
+}
+function allElements() {
+  filter = "all";
+  arr.forEach((todo) => {
+    document.querySelector(`#todo_${todo.id}`).style.display = "flex";
+  });
+}
+
+function activeElements() {
+  allElements();
+  filter = "active";
+  arr.forEach((todo) => {
+    if (todo.complete === true) {
+      document.querySelector(`#todo_${todo.id}`).style.display = "none";
+    }
+  });
+}
+function completedElements() {
+  allElements();
+  filter = "completed";
+  arr.forEach((todo) => {
+    if (todo.complete === false) {
+      document.querySelector(`#todo_${todo.id}`).style.display = "none";
+    }
+  });
+}
+function filterElements() {
+  if (filter === all) {
+    allElements();
+  }
+  if (filter === active) {
+    activeElements();
+  }
+  if (filter === completed) {
+    completedElements();
+  }
+
+  createElement();
+}
+function counts() {
+  let index = 0;
+  arr.forEach((todo) => {
+    if (todo.complete !== true) {
+      index = index + 1;
+    }
+  });
+  return index;
+}
